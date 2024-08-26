d59b793c574a2261e550dcba22c1b9eecc76e048
jose muthuan <josemuthuan@gmail.com>
2024-08-26 09:57:44 -0300

    basic operations (+,-,*,/), parenthesis solving

--- Basics/SimpleCalculator/app/src/main/java/com/example/simplecalculator/ui/CalculatorViewModel.kt
+++ Basics/SimpleCalculator/app/src/main/java/com/example/simplecalculator/ui/CalculatorViewModel.kt
@@ -65,7 +65,7 @@ class CalculatorViewModel: ViewModel() {
     }
 
     private fun isNumber(char: Char): Boolean {
-        return char in '0'..'9'
+        return char in ".0123456789"
     }
 
     fun calculateResult() {
@@ -78,7 +78,7 @@ class CalculatorViewModel: ViewModel() {
 
     fun backspace() {
         if(_uiState.value.currentOperation.last() == '(') {
-            val aux = mapParenthesis.keys
+//            val aux = mapParenthesis.keys
             mapParenthesis.remove(
                 mapParenthesis.filter { element ->
                     element.key[0] == parenthesisCount - 1
@@ -148,45 +148,45 @@ class CalculatorViewModel: ViewModel() {
 
     }
 
-    private fun calculateSubResult(operation: String): Int {
+    private fun calculateSubResult(operation: String): Double {
 
         val result = when(operation.firstOrNull { !isNumber(it) }) {
             'x' -> { multiply(operation) }
             '/' -> { divide(operation)}
             '+' -> { add(operation) }
             '-' -> { subtract(operation)}
-            else -> { operation.toInt() }
+            else -> { operation.toDouble()}
         }
 
         return result
     }
 
-    private fun subtract(operation: String): Int {
+    private fun subtract(operation: String): Double {
         val result = simplify(operation, '-')
 
         return result[0] - result[1]
     }
 
-    private fun add(operation: String): Int {
+    private fun add(operation: String): Double {
         val result = simplify(operation, '+')
 
         return result[0] + result[1]
     }
 
-    private fun divide(operation: String): Int {
+    private fun divide(operation: String): Double {
         val result = simplify(operation, '/')
 
         return result[0] / result[1]
     }
 
 
-    private fun multiply(operation: String): Int {
+    private fun multiply(operation: String): Double {
         val result = simplify(operation, 'x')
 
         return result[0] * result[1]
     }
 
-    private fun simplify(operation: String, operator: Char): Array<Int> {
+    private fun simplify(operation: String, operator: Char): Array<Double> {
 
         val operatorIndex = operation.indexOf(operator)
 
@@ -200,7 +200,7 @@ class CalculatorViewModel: ViewModel() {
             ) {
 
             val i = sub2.indexOfFirst { it in "+-" }
-            val auxOperator = sub2[i]
+//            val auxOperator = sub2[i]
             when(operator) {
                 'x' -> {
                     sub1 =
@@ -216,7 +216,7 @@ class CalculatorViewModel: ViewModel() {
             sub1 =
                 calculateSubResult("${sub1}${sub2.substring(i)}")
                     .toString()
-            return arrayOf(sub1.toInt(), 1)
+            return arrayOf(sub1.toDouble(), 1.0)
         }
 
         if ( !sub2.all { isNumber(it) }) {
@@ -228,20 +228,28 @@ class CalculatorViewModel: ViewModel() {
             sub2 = calculateSubResult(sub2).toString()
         }
 
-        val num1 = sub1.toInt() //operation.substring(startIndex, operatorIndex).toInt()
-        val num2 = sub2.toInt() //operation.substring(operatorIndex + 1, endIndex).toInt()
+        val num1 = sub1.toDouble() //operation.substring(startIndex, operatorIndex).toInt()
+        val num2 = sub2.toDouble() //operation.substring(operatorIndex + 1, endIndex).toInt()
 
         return arrayOf(num1, num2)
     }
  
 
     private fun solveParenthesis() {
-
-        val auxMap = mapParenthesis
+        //check if any parenthesis was left open
         auxOperation = _uiState.value.currentOperation
 
+        if (parenthesisCount != 0) {
+            auxOperation = auxOperation.plus(")".repeat(parenthesisCount))
+
+            repeat(parenthesisCount) { parenthesis() }
+            parenthesisCount = 0
+        }
+
+        var auxMap = mapParenthesis
+
         while (auxMap.isNotEmpty()) {
-            var offset = auxOperation.length
+            val offset = auxOperation.length
             val aux = auxMap.maxBy { it.key[0] }
             var startIndex = 0
             var endIndex = auxOperation.length
@@ -256,15 +264,21 @@ class CalculatorViewModel: ViewModel() {
                     auxOperation.substring(startIndex, endIndex)).toString())
 
             auxMap.remove(aux.key)
-            auxMap.forEach { it ->
-                if(it.value > aux.value) {
-                    auxMap[it.key] = it.value - offset + auxOperation.length
-                }
+
+            val offsetMap = auxMap.toMutableMap()
+
+            auxMap.forEach {
+
                 if(it.key[1] > aux.value) {
-                    auxMap[arrayOf(it.key[0], it.key[1] - offset + auxOperation.length)] = it.value
-                    auxMap.remove(it.key)
+                    offsetMap[arrayOf(it.key[0], it.key[1] - offset + auxOperation.length)] =
+                        it.value - offset + auxOperation.length
+                    offsetMap.remove(it.key)
+                }else if(it.value > aux.value) {
+                    offsetMap[it.key] = it.value - offset + auxOperation.length
                 }
+
             }
+            auxMap = offsetMap.toMutableMap()
         }
     }
 }
